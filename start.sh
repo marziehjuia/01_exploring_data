497316e2f823188e32f6fb942146bc47341d08fe
marziehjuia <marziehj@uia.no>
2025-05-04 12:41:41 +0000

    v11

--- start.sh
+++ start.sh
@@ -4,3 +4,6 @@ jupyter server --ip=0.0.0.0 --port=$PORT \
   --ServerApp.default_url="/voila/render/01_exploring_data.ipynb" \
   --ServerApp.allow_origin='*' \
   --ServerApp.token=''
+  --VoilaConfiguration.kernel_startup_timeout=120
+  --VoilaConfiguration.template=default
+  --VoilaConfiguration.show_tracebacks=True
